9697bb37ac494d35875b0ca0f18123a738ae61f9
hastalafiesta- <mauroscianca98@gmail.com>
2014-10-18 14:56:04 +0200

    script updated

--- repo.sh
+++ repo.sh
@@ -8,6 +8,7 @@ echo -e "Deleting old repos"
 rm -rf $top_build/build
 rm -rf $top_build/device/samsung/janice
 rm -rf $top_build/packages/apps/Settings
+rm -rf $top_build/vendor/pa
 
 echo -e "Repo sync"
 cd $top_build
@@ -18,6 +19,9 @@ cd build
 git fetch https://github.com/hastalafiesta-/android_build.git kitkat
 git cherry-pick 3c71484c3efa476e66c05c7fe953d0948d779083
 git cherry-pick 84a1d1672995a51bd7c6130b0cf7e6e6e2f5511d
+git fetch https://github.com/t4n017/android_build.git kitkat
+git cherry-pick dc0a6596eb93756b2768ed87525540a0eea9aacc
+git cherry-pick 7b22bba3009d6b4ae34db645db290829bf887b61
 
 echo -e "Patching vendor/pa.."
 cd build
